beb0cbfe7ef17c831a62e15f831d3143a3e40dcc
Mike Ackison <mike.ackison@shopify.com>
2021-02-16 16:34:08 -0500

    built the interviewer list with stories

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -14,7 +14,7 @@ export default function DayListItem(props) {
     'day-list__item--selected': props.selected,
     'day-list__item--full': props.spots === 0
 
-  })
+  });
 
   const formatSpots = () => {
     if(props.spots === 0) {
