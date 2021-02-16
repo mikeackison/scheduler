b62cee0f79e1b4af98b26c044f99f26a67817752
Mike Ackison <mike.ackison@shopify.com>
2021-02-15 19:45:17 -0500

    tests working

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -7,6 +7,7 @@ import classNames from 'classnames'
 
 
 export default function DayListItem(props) {
+ 
 
   const dayClass = classNames("day-list__item", {
     
@@ -15,10 +16,20 @@ export default function DayListItem(props) {
 
   })
 
+  const formatSpots = () => {
+    if(props.spots === 0) {
+      return `no spots remaining`
+    } else if (props.spots === 1) {
+      return `${props.spots} spot remaining`
+    } else if (props.spots >= 2) {
+      return `${props.spots} spots remaining`
+    }
+  }
+
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className={"text--regular"}>{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots(props)}</h3>
     </li>
   );
 }
