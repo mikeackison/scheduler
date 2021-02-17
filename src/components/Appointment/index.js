c33297cfd31f2f4073434e41062169f21c980162
Mike Ackison <mike.ackison@shopify.com>
2021-02-17 12:24:28 -0500

    able to display appointmnets seem to be working

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,10 +2,26 @@ import React from "react";
 
 import "components/Appointment/styles.scss";
 
-export default function Appointment() {
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
+
+export default function Appointment(props) {
+
+  // const showAppointment = () => {
+  //   if(props.interview) {
+  //     return <Show/>
+  //   } else{ <Empty/>
+  //   }
+  // }
+  
+  /* ({props.interview}) ? <Show/> : <Empty/> */
 
   return(
-
-    <article className="appointment"></article>
+    <article>
+      <Header time={props.time}  />
+      { (props.interview) ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty /> }
+      
+    </article>
   );
 }
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -172,7 +172,22 @@ storiesOf("InterviewerList", module)
     <Form  name="Some Person" interviewers={interviewers} interviewer={3} onSave={action("onSave")} onCancel={action("onCancel")}/> ))
   .add("Create", () => (
     <Form interviewers={interviewers} onSave={action("onSave")}  onCancel={action("onCancel")}/> ))
- 
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="12pm" />
+      <Appointment id="last" time="1pm" />
+    </Fragment>
+  ))
+  .add("Appointment Booked", () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="12pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+      />
+      <Appointment id="last" time="1pm" />
+    </Fragment>
+  ));
 
 
 
