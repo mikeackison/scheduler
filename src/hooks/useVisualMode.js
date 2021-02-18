f866b5ff70c61bd1f1ad14c729966631081e3a18
Mike Ackison <mike.ackison@shopify.com>
2021-02-18 18:51:29 -0500

    tests passing

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -7,13 +7,29 @@ export default function useVisualMode(initial) {
 
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]); 
-  
-  const transition = (newMode) =>{
+
+  const transition = (newMode, replace = false) =>{
+    if(replace) {
+
     setMode(newMode)
+    } else {
+    setMode(newMode)
+
+    setHistory([...history, newMode])
+    }
   }
 
   const back = () =>{
 
+    if (history.length <= 1) {
+      setMode(initial);
+    } else {
+    const newHistory = [...history.slice(0, -1)];
+
+    setHistory(newHistory);
+
+    setMode(newHistory[newHistory.length -1]);
+    }
   }
 
   return {  mode, transition, back };
