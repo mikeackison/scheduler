c6c05bc8dd1eeacb5b6bbded14d38f668ab4587e
Mike Ackison <mike.ackison@shopify.com>
2021-02-23 18:08:04 -0500

    modified test for validation for no interviewer name, now passing

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -45,9 +45,12 @@ describe('Form', () => {
 
   it('can successfully save after trying to submit an empty student name', () => {
     const onSave = jest.fn();
-    const { getByText, getByPlaceholderText, queryByText } = render(
-      <Form interviewers={interviewers} onSave={onSave} />
-    );
+    const {
+      getByText,
+      getByPlaceholderText,
+      queryByText,
+      getByAltText,
+    } = render(<Form interviewers={interviewers} onSave={onSave} />);
 
     fireEvent.click(getByText('Save'));
 
@@ -58,12 +61,14 @@ describe('Form', () => {
       target: { value: 'Lydia Miller-Jones' },
     });
 
+    fireEvent.click(getByAltText('Sylvia Palmer'));
+
     fireEvent.click(getByText('Save'));
 
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
 
     expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith('Lydia Miller-Jones', null);
+    expect(onSave).toHaveBeenCalledWith('Lydia Miller-Jones', 1);
   });
 
   it('calls onCancel and resets the input field', () => {
@@ -71,7 +76,7 @@ describe('Form', () => {
     const { getByText, getByPlaceholderText, queryByText } = render(
       <Form
         interviewers={interviewers}
-        name='Lydia Mill-Jones'
+        name='Lydia Miller-Jones'
         onSave={jest.fn()}
         onCancel={onCancel}
       />
