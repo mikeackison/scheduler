028118ff6e80b5c615bea52bb6a7d3afb4281cbe
Mike Ackison <mike.ackison@shopify.com>
2021-02-22 19:30:28 -0500

    clear error on succesful submission in validate function

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -22,7 +22,8 @@ export default function Form(props) {
       setError('Student name cannot be blank');
       return;
     }
-
+    // clear the error on successful submission
+    setError('');
     props.onSave(name, interviewer);
   };
 
