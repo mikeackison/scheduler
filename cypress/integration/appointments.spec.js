141edfa2130bc186b6e9547dd38328e2e030c9f2
Mike Ackison <mike.ackison@shopify.com>
2021-02-23 23:58:45 -0500

    all cypress tests passing

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -14,7 +14,7 @@ describe('Appointments', () => {
     cy.contains('Monday');
   });
 
-  xit('should book an interview', () => {
+  it('should book an interview', () => {
     cy.get('[alt=Add]').first().click();
 
     cy.get('[data-testid=student-name-input]').type('Lydia Miller-Jones');
@@ -26,7 +26,7 @@ describe('Appointments', () => {
     cy.contains('.appointment__card--show', 'Sylvia Palmer');
   });
 
-  xit('should edit an interview', () => {
+  it('should edit an interview', () => {
     cy.get('[alt=Edit]').first().click({ force: true });
 
     cy.get('[data-testid=student-name-input]')
